100b9408aac273181622c331075553a4a5261451
OsamaDW <s12028609@stu.najah.edu>
2024-05-25 12:44:42 +0300

    Add edits for drawing line paths on the new canvas

--- script.js
+++ script.js
@@ -37,6 +37,8 @@ function drawPath(ctx, xdes, ydes, xsrc, ysrc){
     ctx.zIndex = 0;
     ctx.moveTo(xsrc, ysrc);  // Move the pen to (100, 100)
     ctx.lineTo(xdes, ydes);
+    ctx.strokeStyle = 'purple';
+    ctx.lineWidth = 2;
     ctx.stroke();
 }
 
@@ -64,27 +66,32 @@ document.addEventListener("DOMContentLoaded", function() {
 
     let vehicle = new Vehicles();
     let simAnneal = new SimulatedAnnealing();
-    const canvas = document.getElementById('canvas');
     const initPath = document.getElementById("initPath");
-    const ctx = canvas.getContext('2d');
-    drawDepot(new Image(), ctx, "Depot.jpg");
-    canvas.addEventListener('click', function(event) {
+    const canvasNodes = document.getElementById('canvasNodes');
+    const ctxNodes = canvasNodes.getContext('2d');
+    const canvasPath = document.getElementById('canvasPath');
+    const ctxPath = canvasPath.getContext('2d');
+
+    drawDepot(new Image(), ctxNodes, "Depot.jpg");
+    canvasNodes.addEventListener('click', function(event) {
                 const rect = this.getBoundingClientRect();
                 // console.log(event.clientY +" " +rect.top)
                 const x = event.clientX - rect.left;
                 const y = event.clientY - rect.top;
                 const weight = Math.floor(Math.random() * 51);
             if(vehicle.addLocationToVehicle(new Locations(0, x, y, 0, 0, weight))) {
-                drawNodes(ctx,x,y,weight);
+                drawNodes(ctxNodes,x,y,weight);
             }else {
                 /*show message that the node is out of boundaries demand or */
             }
 
     });
-    initPath.addEventListener('click', ()=>{
-        simAnneal.drawInitialState(ctx, vehicle.vehicles);
-        document.getElementById("initialDistance").innerHTML = simAnneal.calculateDistance(vehicle.vehicles);
 
+
+    initPath.addEventListener('click', ()=>{
+        canvasNodes.style.zIndex = "1";
+        simAnneal.drawInitialState(ctxPath, vehicle.vehicles);
+        document.getElementById("initialDistance").innerHTML = simAnneal.calculateDistance(vehicle.vehicles);
     });
 });
 
